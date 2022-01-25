e0730d825736ca6b4b210a705caf4742e26d3322
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-01-25 19:10:13 -0300

    Requisito 5

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">
+          {planetName}
+        </p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.defaultProps = {
+  planetName: '',
+  planetImage: '',
+};
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.element,
+  planetImage: PropTypes.string,
+};
+
+export default PlanetCard;
