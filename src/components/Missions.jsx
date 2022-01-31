21297d00c3ecb92105f51630534eeb73ace7f155
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-01-31 00:02:05 -0300

    Requisito 9

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import MissionCard from './MissionCard';
 import Title from './Title';
+import contents from '../data/missions';
 
 class Missions extends React.Component {
   render() {
-    return (<div data-testid="missions"><Title headline="Missões" /></div>);
+    return (
+      <div data-testid="missions">
+        <Title headline="Missões" />
+        {contents.map((content, index) => (
+          <MissionCard
+            name={ content.name }
+            year={ content.year }
+            country={ content.country }
+            destination={ content.destination }
+            key={ index }
+          />
+        ))}
+      </div>);
   }
 }
 
